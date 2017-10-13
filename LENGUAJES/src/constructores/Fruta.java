a130bd65b97341553ee1673c4b40762bd4da427a
T-102 <T-102@PC250>
2017-10-12 19:32:41 -0500

    Clase 12/10/2017 Campos Juarez Jose Eduardo

---
+++ LENGUAJES/src/constructores/Fruta.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package constructores;
+
+/**
+ *
+ * @author T-102
+ */
+public class Fruta {
+    String nombre;
+    float costo;
+    
+    public Fruta(){
+        
+    }
+    
+    public void Fruta(){
+        //Este no es un cotructor es un metodo
+        
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getCosto() {
+        return costo;
+    }
+
+    public void setCosto(float costo) {
+        this.costo = costo;
+    }
+    
+}
---
+++ LENGUAJES/src/constructores/ProbarConstructores.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package constructores;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author T-102
+ */
+public class ProbarConstructores {
+    
+    public static void main(String[] args) {
+    
+     //Creamos un objeto de la clase Animal
+     
+    Animal algo=new Animal();
+    int x = 2;
+ 
+    
+        //algo.setEdad(5);
+        System.out.println(algo.getEdad());
+        System.out.println(algo.getNombre());
+        System.out.println(x);
+        System.out.println(algo.isMuerde());
+        
+        Animal otro=new Animal(8);
+        otro.setEdad(10);
+        System.out.println(otro.getEdad());
+       
+        //vamos a usar un cpnstructor de una clase que no hemos diseñado
+        JFrame ventana=new JFrame("Hola Mundo");
+        
+        ventana.setSize(300,300);
+        ventana.setVisible(true);
+    }
+    
+}
